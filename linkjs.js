02dd9b82cbabfea484c75fb33f866dc7d73b73ba
chandilya194 <74366769+chandilya194@users.noreply.github.com>
2024-11-08 22:59:35 +0530

    Commit

---
+++ linkjs.js
@@ -0,0 +1,33 @@
+const n=document.getElementById("name");
+const url=document.getElementById("linkk");
+const down=document.getElementById("display");
+const c=document.getElementById("closeall");
+
+//if(down.children.length===0){
+//   c.style.display="none";
+//}
+//else{
+//    c.style.display="inline-block";
+//}
+function clr(){
+    down.remove();
+    localStorage.removeItem("data");
+}
+function sub(){
+    
+    let a=document.createElement("a");
+    a.innerHTML=n.value;
+    a.href=url.value;
+    down.appendChild(a);
+    //n.value="";
+    url.value="";
+    save(); 
+}
+function save(){
+     localStorage.setItem("data",down.innerHTML);
+}
+function show(){
+     down.innerHTML=localStorage.getItem("data");
+}
+
+show(); 
